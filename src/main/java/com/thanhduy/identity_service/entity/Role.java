6dd1876e940e840fd4f6a61034ddba812a2726de
Thanh Duy <tranthanhduy08699@gmail.com>
2026-03-13 15:44:43 +0700

    Phan quyen theo role , permission va quan he @Manytomany

--- src/main/java/com/thanhduy/identity_service/entity/Role.java
+++ src/main/java/com/thanhduy/identity_service/entity/Role.java
@@ -1,11 +1,14 @@
 package com.thanhduy.identity_service.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import lombok.*;
 import lombok.experimental.FieldDefaults;
 
-import java.time.LocalDate;
 import java.util.Set;
+
 
 @Entity
 @Getter
@@ -14,18 +17,12 @@ import java.util.Set;
 @NoArgsConstructor
 @AllArgsConstructor
 @FieldDefaults(level = AccessLevel.PRIVATE)
-public class User {
+public class Role {
     @Id
-    @GeneratedValue(strategy = GenerationType.UUID)
-    String id;
-    String username;
-    String password;
-    String firstName;
-    String lastName;
-    LocalDate dob;
+    String name;
+    String description;
 
     @ManyToMany
-    Set<Role> roles;
-
+    Set<Permission> permissions;
 
 }
--- src/main/java/com/thanhduy/identity_service/mapper/PermissionMapper.java
+++ src/main/java/com/thanhduy/identity_service/mapper/PermissionMapper.java
@@ -1,17 +1,18 @@
 package com.thanhduy.identity_service.mapper;
 
+import com.thanhduy.identity_service.dto.request.PermissionRequest;
 import com.thanhduy.identity_service.dto.request.UserCreationRequest;
 import com.thanhduy.identity_service.dto.request.UserUpdateRequest;
+import com.thanhduy.identity_service.dto.response.PermissionResponse;
 import com.thanhduy.identity_service.dto.response.UserResponse;
+import com.thanhduy.identity_service.entity.Permission;
 import com.thanhduy.identity_service.entity.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 @Mapper(componentModel = "spring")
-public interface UserMapper {
-    User toUser(UserCreationRequest request);
-    UserResponse toUserResponse(User user);
-    @Mapping(target = "roles", ignore = true)
-    void updateUser(@MappingTarget User user, UserUpdateRequest request);
+public interface PermissionMapper {
+    Permission toPermission(PermissionRequest request);
+    PermissionResponse toPermissionResponse(Permission permission);
 }
